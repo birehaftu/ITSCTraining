bdc7dd6dad51f8fe1357e627fda7c2416df3ce5d
birehaftu <birehaftu@gmail.com>
2023-09-26 12:12:45 +0300

    commit

--- APIClass/APIRequest.py
+++ APIClass/APIRequest.py
@@ -1,6 +1,6 @@
 import requests
 #url="http://127.0.0.1:8000/my-second-api?name=birhane "
-url="http://127.0.0.1:8000/my-getdata-api"
+url="http://127.0.0.1:8087/my-getdata-api"
 response=requests.get(url)
 ##try:
 #print(response.content)#display content
